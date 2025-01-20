fd47b74f01d36da0e7f41567c1c68cd2bbab50fb
Manish Jangir <111438326+manish201258@users.noreply.github.com>
2025-01-20 09:06:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ app.use("/api/auth", router);
 
 connectDb().then(() => {
   const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on port ${port}`);
   });
 });
+
